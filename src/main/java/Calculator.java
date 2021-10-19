22e01cc0c21c0f89a1e2e32270cf35a0dc99bfb2
Дима <richardbatyrov.gmail.com>
2021-10-19 19:40:53 +0300

    .

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class Calculator {
     public static void main(String[] args) {
         Scanner scan= new Scanner(System.in);
-        System.out.println("Введите уравнение");
+        System.out.print("Введите уравнение: ");
         String expr= scan.nextLine();
         String[] words= expr.split(" ");
         if(words.length!=3) throw new RuntimeException("Некорректная форма ввода");
@@ -17,11 +17,9 @@ public class Calculator {
         try {
             int num11= Integer.parseInt(num1);
             int num22= Integer.parseInt(num2);
-            if((num11>=1&&num11<=10)&&(num22>=1&&num22<=10)){
-                int result= Operation.solve(num11, num22, oper);
-                System.out.printf("Результат равен %d", result);
-            }
-            else { throw new RuntimeException("Числа должны быть от 1 до 10");}
+            if((num11<1||num11>10)||(num22<1||num22>10)){ throw new RuntimeException();}
+            else {int result= Operation.solve(num11, num22, oper);
+                System.out.printf("Результат равен %d", result);}
         }
         catch (Exception ex) {
             List<RomanNum> romanNumList= RomanNum.getValues();
@@ -33,9 +31,10 @@ public class Calculator {
                 if (num2.equals(romanNumList.get(i).name())) {num2Arab=i+1;}
                 i++;
             }
-            System.out.println(num1Arab);
+            if (num1Arab==0||num2Arab==0) throw new RuntimeException("Некорректная форма ввода");
             int result= Operation.solve(num1Arab, num2Arab, oper);
-            if (result<1) throw new RuntimeException("Римское число должно быть больше нуля");
+            if (result<0) throw new RuntimeException("В римской системе счисления нет отрицательных чисел");
+            if (result==0) throw new RuntimeException("В римской системе счисления нет нуля");
             String romanStringResult= RomanNum.getRomResult(result);
             System.out.printf("Результат равен %s",romanStringResult);
 
